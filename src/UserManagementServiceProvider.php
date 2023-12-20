f0c6a0776d99354385d5a75eb632ec5cb1704dc3
iananta <ianantashrestha@gmail.com>
2023-12-20 23:17:32 +0545

    update load file

--- src/UserManagementServiceProvider.php
+++ src/UserManagementServiceProvider.php
@@ -4,6 +4,7 @@ use Illuminate\Support\ServiceProvider;
 class UserManagementServiceProvider extends ServiceProvider{
 	public function boot(){
 		$this->loadMigrationsFrom(__DIR__.'/database/migrations');
+		$this->loadMigrationsFrom(__DIR__.'/database/seeders');
 	}
 
 	public function register(){
